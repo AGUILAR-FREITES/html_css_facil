e07b43e8b39184b0f19cbe33102765c53788185f
AGUILAR-FREITES <aguilarfreites@gmail.com>
2025-11-04 23:08:39 +0100

    Clase de JavaScript 1

---
+++ ATR11/Clases_JavaScript/variables_constantes.js
@@ -0,0 +1,15 @@
+document.getElementById("root").innerHTML = "Hello World !!!!"
+
+
+
+
+
+let numb= 44;
+let letras = "abecedario";
+let afirmando = true;
+const piNum = 3.14;
+
+
+console.log(piNum);
+console.log("Las letras son del ", letras);
+console.log("el sol es de color amarillo ", afirmando);
